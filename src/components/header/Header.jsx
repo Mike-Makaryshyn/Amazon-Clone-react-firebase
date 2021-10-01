988de5690644fb771c078d931646ada95db59bc5
Mike Makaryshyn <mikemakaryshyn@gmail.com>
2021-10-01 22:05:01 +0300

    initial

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Header.css";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdShoppingBasket } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { useStateValue } from "../../StateProvider";
+
+const Header = () => {
+   const [{ basket }, dispatch] = useStateValue();
+
+   return (
+      <div className="header">
+         <Link to="/">
+            <img
+               className="header__logo"
+               src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+               alt="amazon-logo"
+            />
+         </Link>
+
+         <div className="header__search">
+            <input type="text" className="header__search-input" />
+            <AiOutlineSearch className="header__search-icon" />
+         </div>
+
+         <div className="header__nav">
+            <div className="header__option">
+               <span className="header__option-fl">Hello</span>
+               <span className="header__option-sl">Sign In</span>
+            </div>
+            <div className="header__option">
+               {" "}
+               <span className="header__option-fl">Returns</span>
+               <span className="header__option-sl"> & Orders</span>
+            </div>
+            <div className="header__option">
+               {" "}
+               <span className="header__option-fl">Your</span>
+               <span className="header__option-sl">Prime </span>
+            </div>
+
+            <Link to="/checkout">
+               <div className="header__option-basket">
+                  <MdShoppingBasket className="header__option-basket-icon" />
+                  <span className="header__option-sl header__basket-count">
+                     {basket?.length}
+                  </span>
+               </div>
+            </Link>
+         </div>
+      </div>
+   );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyBRUQikPy9SmkpTFyZooxTQ3Cc6lPwMLTA",
+   authDomain: "clone-81844.firebaseapp.com",
+   projectId: "clone-81844",
+   storageBucket: "clone-81844.appspot.com",
+   messagingSenderId: "641847497365",
+   appId: "1:641847497365:web:842b39da3f317b7d4b019c"
+ };
